f194db76b3db5d23df46ccb79f30dea878b88ad8
Dana Hartweg <dana.hartweg@formstack.com>
2015-10-12 16:08:35 -0400

    Routing framework.

--- server.js
+++ server.js
@@ -5,10 +5,58 @@
       bodyParser = require('body-parser'),
       cors       = require('cors');
 
-  var server = express();
+  var server = express(),
+      router = express.Router();
 
   server.use(bodyParser.json());
   server.use(cors());
 
+  // let's display the readme
+  server.get('/', function(req, res) {
+    res.send('Main page.');
+  });
+
+  // REST API ROUTING
+  // ----------------
+
+  // ls
+  router.route('/ls')
+
+  .get(function(req, res) {
+    res.json({ message: 'test all' });
+  });
+
+  router.route('/ls/:id')
+
+  .get(function(req, res) {
+    res.json({ message: 'id: ' + req.params.id });
+  });
+
+  // cat
+  router.route('/cat')
+
+  .get(function(req, res) {
+    res.json({ message: 'test all' });
+  });
+
+  router.route('/cat/:id')
+
+  .get(function(req, res) {
+    res.json({ message: 'id: ' + req.params.id });
+  });
+
+  // autocomplete
+  router.route('/autocomplete/:text')
+
+  .get(function(req, res) {
+    res.json({ message: 'id: ' + req.params.text });
+  });
+
+  // -----------
+  // END ROUTING
+
+  server.use('/api', router);
   server.listen(8080);
+
+  console.log('FS Front-End Applicant Project server running on 8080');
 })();
